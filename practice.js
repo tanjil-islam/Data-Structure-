f62f30169cf06d00f8a8e6856f2d0d21452f9cac
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2022-07-04 20:36:36 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,14 @@
+const nemo=['nemo'];
+
+function findNemo(array)
+  {
+    for(let i=0; i<array.length; i++)
+      {
+        if(array[i]==='nemo')
+        {
+          console.log('Found Nemo!');
+        }
+      }
+  }
+
+findNemo(nemo);
