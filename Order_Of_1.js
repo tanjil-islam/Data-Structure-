e1b3d65ebe0ba318f4cc318e2b1f24b481fdbe25
tanjil-islam <tanjil.islam040820@gmail.com>
2022-07-22 11:27:22 +0600

    THis programme is Big O(1)basic.

---
+++ Order_Of_1.js
@@ -0,0 +1,11 @@
+const boxes=[0,6,2,,4,5];
+
+function logFirstThreeboxes(boxes)
+{
+    console.log(boxes[0]);
+    console.log(boxes[1]);
+    console.log(boxes[2]);
+
+}
+
+logFirstThreeboxes(boxes);
