f969f9eebae95aed9190f6f8e9f5d3ed6adee9db
tanjil-islam <tanjil.islam040820@gmail.com>
2022-07-23 00:10:38 +0600

    This is mixed of order1 and order n basic.

---
+++ Order_Of_1_And_n.js
@@ -0,0 +1,27 @@
+function anotherFunChallenge(input)
+{
+    let a=10;
+    let b=20;
+    let =30;
+
+    for(let i=0; i<input; i++)
+    {
+        let x=i+1;
+        let y=i+2;
+        let z=i+3;
+
+        console.log("Z = "+z);
+
+    }
+
+    for(let j=0; j<input; j++)
+    {
+        let p=j*2;
+        let q=j*2;
+        console.log("Q = "+q);
+    }
+
+    let whoAmI="I don't know";
+}
+
+anotherFunChallenge(3);
