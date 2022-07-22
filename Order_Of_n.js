16aedf2f801f6da4ea8e528aaf0ddd8fdbc8779d
tanjil-islam <tanjil.islam040820@gmail.com>
2022-07-22 15:02:09 +0600

    This is Order of n basic.

---
+++ Order_Of_n.js
@@ -0,0 +1,16 @@
+function funChallenge(input)
+{
+    let a=10;
+    a=50+3;
+
+    for(let i=0; i<input.length,i++;)
+    {
+        anotherfunction();
+        let stranger=true;
+        a++;
+
+    }
+    console.log(a);
+    
+}
+funChallenge(4);
